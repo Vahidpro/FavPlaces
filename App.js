ca6bbbe76160d7ab34215a677fec607b9baf8c9b
Vahid Porki <ivahidp@gmail.com>
2023-02-11 08:27:12 +0330

    Add Navigation

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import "react-native-gesture-handler";
+import AllPlaces from "./screens/AllPlaces";
+import AddPlace from "./screens/AddPlace";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const Stack = createStackNavigator();
+	return (
+		<>
+			<StatusBar style="auto" />
+			<NavigationContainer>
+				<Stack.Navigator>
+					<Stack.Screen name="AllPlaces" component={AllPlaces}></Stack.Screen>
+					<Stack.Screen name="AddPlace" component={AddPlace}></Stack.Screen>
+				</Stack.Navigator>
+			</NavigationContainer>
+		</>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
