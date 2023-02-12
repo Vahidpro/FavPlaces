6b93245ebf62f2fb99da420951605c6b5c62612b
Vahid Porki <ivahidp@gmail.com>
2023-02-12 09:36:52 +0330

    Take photos and show preview

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,18 +1,54 @@
-import { Button, StyleSheet, View } from "react-native";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
 import { launchCameraAsync } from "expo-image-picker";
+import { useState } from "react";
+import { Colors } from "../../constants/colors";
 
 function ImagePicker() {
+	const [pickedImageUri, setPickedImageUri] = useState();
+	// iOS permission
+	// function ImagePicker() {
+	// 	const [camPermissionInfo, requestPermission] = useCameraPermission();
+
+	// 	async function verifyPermissions() {
+	// 		if (camPermissionInfo.status === PermissionStatus.UNDETERMINED) {
+	// 			const permissionResponse = await requestPermission();
+
+	// 			return permissionResponse.granted;
+	// 		}
+	// 		if (camPermissionInfo.status === PermissionStatus.DENIED) {
+	// 			Alert.alert(
+	// 				"Insufficient Permissions!",
+	// 				"You need to grant camera permission to use this app."
+	// 			);
+
+	// 			return false;
+	// 		}
+	// 		return true;
+	// 	}
+
 	async function takeImageHandler() {
+		// iOS
+		// const hasPermission = await verifyPermissions();
+
+		// if (!hasPermission) return;
 		const image = await launchCameraAsync({
 			allowsEditing: true,
 			aspect: [16, 9],
 			quality: 0.5,
 		});
-		console.log(image);
+		setPickedImageUri(image.uri);
+	}
+
+	let imagePreview = <Text>No image taken yet.</Text>;
+
+	if (pickedImageUri) {
+		imagePreview = (
+			<Image style={styles.image} source={{ uri: pickedImageUri }}></Image>
+		);
 	}
 	return (
 		<View style={styles.container}>
-			<View></View>
+			<View style={styles.imagePreview}>{imagePreview}</View>
 			<Button title="Take Image" onPress={takeImageHandler}></Button>
 		</View>
 	);
@@ -20,7 +56,18 @@ function ImagePicker() {
 export default ImagePicker;
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
+	imagePreview: {
+		width: "100%",
+		height: 200,
+		marginVertical: 8,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: Colors.primary100,
+		borderRadius: 8,
+	},
+	image: {
+		width: "100%",
+		height: "100%",
+		borderRadius: 8,
 	},
 });
